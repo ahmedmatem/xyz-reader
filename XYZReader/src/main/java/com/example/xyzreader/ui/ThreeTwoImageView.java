4216c06bc148e3e6f3b7f013e7201b8e9cf1dbec
Ahmed Matem Ahmed <ahmedmatem@gmail.com>
2018-02-04 21:52:54 +0000

    work image bleed

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ThreeTwoImageView.java
@@ -0,0 +1,32 @@
+package com.example.xyzreader.ui;
+
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by ahmed on 04/02/2018.
+ */
+
+public class ThreeTwoImageView extends android.support.v7.widget.AppCompatImageView {
+    public ThreeTwoImageView(Context context) {
+        super(context);
+    }
+
+    public ThreeTwoImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ThreeTwoImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int threeTwoHeight = MeasureSpec.getSize(widthMeasureSpec) * 2 / 3;
+        int threeTwoHeightSpec = MeasureSpec.makeMeasureSpec(threeTwoHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, threeTwoHeightSpec);
+    }
+}
